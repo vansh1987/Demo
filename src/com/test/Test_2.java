c2bbec8548d344722cbd4944ec6c431d34c9afee
deepanshu <dipanshu@codelee.com>
2018-01-18 15:45:02 +0530

    Testing the branch creation and checkout

--- src/com/test/Test_2.java
+++ src/com/test/Test_2.java
@@ -8,6 +8,6 @@ public class Test_2
     public static void main (String[] args)
     {
         System.out.println("Test2");
-
+        System.out.println("Testing the new branch creation");
     }
 }
