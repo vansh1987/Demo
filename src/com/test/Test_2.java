1545345f13e17f101f02033d43086dc7e1eeae54
deepanshu <dipanshu@codelee.com>
2018-01-18 13:36:10 +0530

    Adding single file testing from git tool

---
+++ src/com/test/Test_2.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Created by Demo on 1/18/2018.
+ */
+public class Test_2
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Test2");
+
+    }
+}
